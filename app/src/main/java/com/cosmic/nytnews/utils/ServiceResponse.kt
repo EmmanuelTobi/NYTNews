608b28e8d99f89000d4f8521d536b58879a53657
Emmanuel Tobi <emmanuel.a.oluwatobi@gmail.com>
2025-02-08 06:40:26 +0100

    update state loading class

--- app/src/main/java/com/cosmic/nytnews/utils/ServiceResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.cosmic.nytnews.utils
-
-sealed class ServiceResponse<out T> {
-    data class Success<out T>(val data : T) : ServiceResponse<T>()
-    data class Error(val message : String) : ServiceResponse<Nothing>()
-    data object Loading : ServiceResponse<Nothing>()
-}
