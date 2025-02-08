9585fc6c01f7af42711a4552fd73e80eadd6eb87
Emmanuel Tobi <emmanuel.a.oluwatobi@gmail.com>
2025-02-08 07:55:57 +0100

    add buttomsheet to view more details during onclick, also add image loading

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,5 +78,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("io.coil-kt:coil-compose:2.4.0")
 
 }
--- app/src/main/java/com/cosmic/nytnews/MainActivity.kt
+++ app/src/main/java/com/cosmic/nytnews/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.cosmic.nytnews
 
-import android.os.Build
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -23,36 +26,45 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.cosmic.nytnews.ui.theme.NYTNewsTheme
-import com.cosmic.nytnews.viewmodel.NewsViewmodel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.request.CachePolicy
+import coil.request.ImageRequest
+import coil.transform.RoundedCornersTransformation
+import com.cosmic.nytnews.model.NYTimesNewsModel
+import com.cosmic.nytnews.ui.theme.NYTNewsTheme
 import com.cosmic.nytnews.utils.AppDateUtils
 import com.cosmic.nytnews.utils.ResultState
-
+import com.cosmic.nytnews.viewmodel.NewsViewmodel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -98,6 +110,8 @@ class MainActivity : ComponentActivity() {
 fun Greeting(newsViewModel: NewsViewmodel = viewModel()) {
 
     val posts by newsViewModel.getNewsPosts.observeAsState()
+    var selectedNewsItem by remember { mutableStateOf<NYTimesNewsModel.Result?>(null) }
+    var showBottomSheet by remember { mutableStateOf(false) }
 
     posts.let {
         when(it) {
@@ -108,7 +122,12 @@ fun Greeting(newsViewModel: NewsViewmodel = viewModel()) {
                     contentPadding = PaddingValues(16.dp)
                 ) {
                     items(it.data.results) { item ->
-                        Column {
+                        Column (
+                            modifier = Modifier.clickable {
+                                selectedNewsItem = item
+                                showBottomSheet = true
+                            }
+                        ) {
                             NewsPostListItem(
                                 item.title,
                                 item.abstract,
@@ -134,6 +153,10 @@ fun Greeting(newsViewModel: NewsViewmodel = viewModel()) {
             }
             null -> TODO()
         }
+    }
+
+    if (showBottomSheet && selectedNewsItem != null) {
+        NewsItemBottomSheet(selectedNewsItem = selectedNewsItem!!, onDismiss = {showBottomSheet = false})
     }
 
 }
@@ -235,6 +258,97 @@ fun LoadingView() {
             )
             Spacer(modifier = Modifier.height(16.dp))
             Text(text = "Loading...", fontWeight = FontWeight.Bold)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NewsItemBottomSheet(selectedNewsItem: NYTimesNewsModel.Result, onDismiss: () -> Unit) {
+    val context = LocalContext.current
+    val sheetState = rememberModalBottomSheetState()
+
+    ModalBottomSheet(
+        onDismissRequest = {
+            onDismiss()
+        },
+        sheetState = sheetState,
+        dragHandle = null,
+        windowInsets = WindowInsets(0,0,0,0),
+        modifier = Modifier.background(Color.Transparent)
+    ) {
+        // Sheet content
+        Column(modifier = Modifier.padding(16.dp)) {
+            Spacer(modifier = Modifier.height(4.dp))
+            if(selectedNewsItem.media.isNotEmpty()) {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(selectedNewsItem.media[0].mediaMetadata[selectedNewsItem.media[0].mediaMetadata.size - 1].url)
+                        .memoryCachePolicy(CachePolicy.ENABLED)
+                        .diskCachePolicy(CachePolicy.ENABLED)
+                        .transformations(RoundedCornersTransformation(16.dp.value))
+                        .build(),
+                    contentDescription = "News Image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .clip(RoundedCornerShape(16.dp)),
+                    onState = { state ->
+                        when (state) {
+                            is AsyncImagePainter.State.Error -> {
+                                // Handle the error, e.g., display an error image
+                                Log.e("AsyncImage", "Error loading image: ${state.result.throwable}")
+                            }
+
+                            is AsyncImagePainter.State.Loading -> {
+                                Log.e("AsyncImage", "Loading image")
+                            }
+
+                            is AsyncImagePainter.State.Success -> {
+                                // Image loaded successfully
+                            }
+
+                            is AsyncImagePainter.State.Empty -> {
+                                // Handle the empty state
+                                Log.e("AsyncImage", "Empty image state")
+                            }
+                        }
+                    },
+                )
+            }
+            Spacer(modifier = Modifier.height(34.dp))
+            Text(
+                text = selectedNewsItem.title,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = selectedNewsItem.abstract,
+                fontSize = 16.sp
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = AppDateUtils.formatDateToReadableWords(selectedNewsItem.publishedDate),
+                    fontSize = 14.sp,
+                    color = Color.Gray
+                )
+                OutlinedButton(
+                    onClick = {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(selectedNewsItem.url))
+                        context.startActivity(intent)
+                    }
+                ) {
+                    Text(text = "Read more")
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
