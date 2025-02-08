aad2fcbc0dd418945fb88c4f9cc1612656031850
Emmanuel Tobi <emmanuel.a.oluwatobi@gmail.com>
2025-02-08 06:41:13 +0100

    use api result state class with api calls

--- app/src/main/java/com/cosmic/nytnews/viewmodel/NewsViewmodel.kt
+++ app/src/main/java/com/cosmic/nytnews/viewmodel/NewsViewmodel.kt
@@ -1,89 +1,81 @@
 package com.cosmic.nytnews.viewmodel
 
 import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.cosmic.nytnews.model.NYTimesNewsModel
 import com.cosmic.nytnews.repository.NewsRepository
+import com.cosmic.nytnews.utils.ResultState
 import kotlinx.coroutines.launch
 
 class NewsViewmodel : ViewModel() {
     val TAG = "NewsViewmodel"
     private val repository = NewsRepository()
 
-    private val _error = MutableLiveData<String>()
-    val error: LiveData<String> = _error
+    private val _posts = MutableLiveData<ResultState<NYTimesNewsModel>>(ResultState.Loading)
+    val getNewsPosts: MutableLiveData<ResultState<NYTimesNewsModel>> = _posts
 
     init {
         fetchNewsPostsLast1Day()
     }
 
-    private val _posts = MutableLiveData<NYTimesNewsModel>()
-    val getNewsPosts: LiveData<NYTimesNewsModel> = _posts
-
-    private fun fetchNewsPostsLast1Day() {
+    fun fetchNewsPostsLast1Day() {
+        _posts.value = ResultState.Loading
         viewModelScope.launch {
             try {
-
                 val response = repository.getPostsLast1Day()
                 response.collect {
                     if(it.status == "OK") {
-                        _posts.value = it
-                        Log.d(TAG, "fetchNewsPostsLast1Day: ${it.status}")
+                        _posts.value = ResultState.Success(it)
                     } else {
-                        _error.value = "Error: Unable to get news post at the moment"
+                        _posts.value = ResultState.Error("Error: Unable to get news post at the moment")
                     }
                 }
 
             } catch (e: Exception) {
                 Log.d(TAG, e.message.toString())
-                _error.value = "Exception: ${e.message}"
+                _posts.value = ResultState.Error(e.message.toString())
             }
         }
     }
 
-    private val _sevenDaysNewPosts = MutableLiveData<NYTimesNewsModel>()
-    val get7daysNewsPosts: LiveData<NYTimesNewsModel> = _posts
-
-    private fun fetchNewsPostsLast7Days() {
+    fun fetchNewsPostsLast7Days() {
+        _posts.value = ResultState.Loading
         viewModelScope.launch {
             try {
 
                 val response = repository.getPostsLast7Days()
                 response.collect {
                     if(it.status == "OK") {
-                        _sevenDaysNewPosts.value = it
+                        _posts.value = ResultState.Success(it)
                     } else {
-                        _error.value = "Error: Unable to get news post at the moment"
+                        _posts.value = ResultState.Error("Error: Unable to get news post at the moment")
                     }
                 }
 
             } catch (e: Exception) {
-                _error.value = "Exception: ${e.message}"
+                _posts.value = ResultState.Error(e.message.toString())
             }
         }
     }
 
-    private val _thirtyDaysNewPosts = MutableLiveData<NYTimesNewsModel>()
-    val getThirtyDaysNewsPosts: LiveData<NYTimesNewsModel> = _posts
-
-    private fun fetchNewsPostsLast30Days() {
+    fun fetchNewsPostsLast30Days() {
+        _posts.value = ResultState.Loading
         viewModelScope.launch {
             try {
 
                 val response = repository.getPostsLast7Days()
                 response.collect {
                     if(it.status == "OK") {
-                        _thirtyDaysNewPosts.value = it
+                        _posts.value = ResultState.Success(it)
                     } else {
-                        _error.value = "Error: Unable to get news post at the moment"
+                        _posts.value = ResultState.Error("Error: Unable to get news post at the moment")
                     }
                 }
 
             } catch (e: Exception) {
-                _error.value = "Exception: ${e.message}"
+                _posts.value = ResultState.Error(e.message.toString())
             }
         }
     }
